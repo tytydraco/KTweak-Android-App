b37c3d66d7069aa2ddc1793dffac5f6cb8bd8451
tytydraco <tylernij@gmail.com>
2020-09-21 22:06:31 -0700

    Fix boot service not working sometimes
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/ktweak/Receivers/BootReceiver.kt
+++ app/src/main/java/com/draco/ktweak/Receivers/BootReceiver.kt
@@ -3,23 +3,17 @@ package com.draco.ktweak.Receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.preference.PreferenceManager
-import com.draco.ktweak.R
-import com.draco.ktweak.Utils.KTweak
+import android.os.Build
+import com.draco.ktweak.Services.BootService
 
 class BootReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-
-            if (prefs.getBoolean(context.getString(R.string.pref_apply_on_boot), true)) {
-                val ktweak = KTweak(context)
-                val autoFetch = prefs.getBoolean(context.getString(R.string.pref_auto_fetch), true)
-                if (autoFetch)
-                    ktweak.updateScript { ktweak.execute() }
-                else
-                    ktweak.execute()
-            }
+            val bootServiceIntent = Intent(context, BootService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                context.startForegroundService(bootServiceIntent)
+            else
+                context.startService(bootServiceIntent)
         }
     }
 }
---
+++ app/src/main/java/com/draco/ktweak/Services/BootService.kt
@@ -0,0 +1,29 @@
+package com.draco.ktweak.Services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.preference.PreferenceManager
+import com.draco.ktweak.R
+import com.draco.ktweak.Utils.KTweak
+
+class BootService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        if (prefs.getBoolean(getString(R.string.pref_apply_on_boot), true)) {
+            val ktweak = KTweak(this)
+            val autoFetch = prefs.getBoolean(getString(R.string.pref_auto_fetch), true)
+            if (autoFetch)
+                ktweak.updateScript { ktweak.execute() }
+            else
+                ktweak.execute()
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
