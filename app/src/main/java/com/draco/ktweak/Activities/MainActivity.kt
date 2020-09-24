c9c2cccc6563723db30888a9ef99ae4292228a36
tytydraco <tylernij@gmail.com>
2020-09-24 14:47:38 -0700

    Always auto fetch
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/ktweak/Activities/MainActivity.kt
+++ app/src/main/java/com/draco/ktweak/Activities/MainActivity.kt
@@ -3,16 +3,10 @@ package com.draco.ktweak.Activities
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.draco.ktweak.Fragments.MainPreferenceFragment
 import com.draco.ktweak.R
-import com.draco.ktweak.Utils.Script
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
-    private val script = Script(this)
-
     private fun isRooted(): Boolean {
         val rootCheckProcess = ProcessBuilder("which", "su").start()
         rootCheckProcess.waitFor()
@@ -25,51 +19,9 @@ class MainActivity : AppCompatActivity() {
         return rootCheckProcess.exitValue() == 0
     }
 
-    private fun fetch() {
-        val ret = script.fetch()
-        runOnUiThread {
-            when (ret) {
-                Script.Companion.FetchStatus.SUCCESS -> {
-                    Snackbar.make(
-                        swipeRefreshLayout, getString(R.string.snackbar_fetch_success),
-                        Snackbar.LENGTH_SHORT
-                    )
-                        .setAction(getString(R.string.snackbar_dismiss)) {}
-                        .show()
-                }
-
-                Script.Companion.FetchStatus.FAILURE -> {
-                    Snackbar.make(
-                        swipeRefreshLayout, getString(R.string.snackbar_fetch_failure),
-                        Snackbar.LENGTH_SHORT
-                    )
-                        .setAction(getString(R.string.snackbar_dismiss)) {}
-                        .show()
-                }
-
-                Script.Companion.FetchStatus.UNCHANGED -> {
-                    Snackbar.make(
-                        swipeRefreshLayout, getString(R.string.snackbar_fetch_unchanged),
-                        Snackbar.LENGTH_SHORT
-                    )
-                        .setAction(getString(R.string.snackbar_dismiss)) {}
-                        .show()
-                }
-            }
-            swipeRefreshLayout.isRefreshing = false
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        swipeRefreshLayout = findViewById(R.id.main_layout)
-        swipeRefreshLayout.setOnRefreshListener {
-            Thread {
-                fetch()
-            }.start()
-        }
 
         /* Setup preference screen */
         supportFragmentManager
