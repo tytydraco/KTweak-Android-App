7be3dad367c0858dde832def88a8545aaaa9411d
tytydraco <tylernij@gmail.com>
2020-09-28 20:27:36 -0700

    Remove unused imports
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/ktweak/activities/MainActivity.kt
+++ app/src/main/java/com/draco/ktweak/activities/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.draco.ktweak.activities
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.draco.ktweak.fragments.MainPreferenceFragment
 import com.draco.ktweak.R
-import com.draco.ktweak.services.BootService
+import com.draco.ktweak.fragments.MainPreferenceFragment
 
 class MainActivity : AppCompatActivity() {
     private fun isRooted(): Boolean {
