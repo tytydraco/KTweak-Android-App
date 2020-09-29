497a4e7ab6275f857cc300025ad8bd50b7e5a902
tytydraco <tylernij@gmail.com>
2020-09-28 20:28:46 -0700

    Simplify root check
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/ktweak/activities/MainActivity.kt
+++ app/src/main/java/com/draco/ktweak/activities/MainActivity.kt
@@ -8,12 +8,6 @@ import com.draco.ktweak.fragments.MainPreferenceFragment
 
 class MainActivity : AppCompatActivity() {
     private fun isRooted(): Boolean {
-        val rootCheckProcess = ProcessBuilder("which", "su").start()
-        rootCheckProcess.waitFor()
-        return rootCheckProcess.exitValue() == 0
-    }
-
-    private fun rootGranted(): Boolean {
         val rootCheckProcess = ProcessBuilder("su", "-c", "exit").start()
         rootCheckProcess.waitFor()
         return rootCheckProcess.exitValue() == 0
@@ -31,12 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         /* Warn user on each start if root is not detected */
         if (!isRooted()) {
-            AlertDialog.Builder(this)
-                .setTitle(getString(R.string.alert_not_rooted_title))
-                .setMessage(getString(R.string.alert_not_rooted_message))
-                .setPositiveButton(getString(R.string.alert_confirm), null)
-                .show()
-        } else if (!rootGranted()) {
             AlertDialog.Builder(this)
                 .setTitle(getString(R.string.alert_root_not_granted_title))
                 .setMessage(getString(R.string.alert_root_not_granted_message))
