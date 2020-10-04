aee8548e9878fbbfeda8a22c4702db93bd8d4fb7
tytydraco <tylernij@gmail.com>
2020-10-03 19:26:10 -0700

    Add share button to logs
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/ktweak/activities/LogActivity.kt
+++ app/src/main/java/com/draco/ktweak/activities/LogActivity.kt
@@ -1,13 +1,16 @@
 package com.draco.ktweak.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.draco.ktweak.BuildConfig
 import com.draco.ktweak.utils.Script
 import com.draco.ktweak.adapters.LogRecyclerAdapter
 import com.draco.ktweak.R
@@ -48,11 +51,11 @@ class LogActivity: AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.logs, menu)
 
         /* Clear logs */
-        menu!!.findItem(R.id.clear_logs).setOnMenuItemClickListener {
+        menu.findItem(R.id.clear_logs).setOnMenuItemClickListener {
             val log = File(filesDir, Script.logName)
 
             if (!log.exists()) {
@@ -62,6 +65,35 @@ class LogActivity: AppCompatActivity() {
             } else {
                 log.delete()
                 finish()
+            }
+            return@setOnMenuItemClickListener true
+        }
+
+        /* Share log file */
+        menu.findItem(R.id.share).setOnMenuItemClickListener {
+            val log = File(filesDir, Script.logName)
+
+            if (!log.exists()) {
+                Snackbar.make(recyclerView, getString(R.string.snackbar_share_failure), Snackbar.LENGTH_SHORT)
+                    .setAction(getString(R.string.snackbar_dismiss)) {}
+                    .show()
+            } else {
+                try {
+                    val uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", log)
+                    val intent = Intent(Intent.ACTION_SEND)
+                    with (intent) {
+                        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                        putExtra(Intent.EXTRA_STREAM, uri)
+                        type = "*/*"
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    }
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    Snackbar.make(recyclerView, getString(R.string.snackbar_intent_failed), Snackbar.LENGTH_SHORT)
+                        .setAction(getString(R.string.snackbar_dismiss)) {}
+                        .show()
+                }
             }
             return@setOnMenuItemClickListener true
         }
